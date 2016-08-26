4456ae5a19dcb43c9d1b57a519873d678bf3e976
bphang@ucsd.edu <spis16ag@acs-cseb240-30.ucsd.edu>
2016-08-26 15:16:18 -0700

    hey

--- hello.py
+++ hello.py
@@ -5,5 +5,18 @@ app = Flask(__name__)
 def hello():
     return "Hey dude"
 
+@app.route('/ftoc')
+def renderResult():
+    try:
+        print "request.args['fTemp']",request.args['fTemp']
+        ftemp = float(request.args['fTemp'])
+        ctemp = ftoc(ftemp)
+        return render_template('celsiusResult.html', fTemp=ftemp, cTemp=ctemp)
+    except ValueError:
+        return "Sorry: something went wrong."
+
+def ftoc(ftemp):
+   return (ftemp-32.0)*(5.0/9.0)
+
 if __name__ == "__main__":
     app.run(debug=False)
